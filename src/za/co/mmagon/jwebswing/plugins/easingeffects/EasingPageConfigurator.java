ad35bb6eab07f857f8e92fde34258392ff92f49b
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:56 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/easingeffects/EasingPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/easingeffects/EasingPageConfigurator.java
@@ -1,25 +1,18 @@
-/*
- * The MIT License
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * Copyright 2017 GedMarc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.easingeffects;
 
@@ -33,15 +26,23 @@ import za.co.mmagon.jwebswing.plugins.PluginInformation;
  * @since 26 Feb 2017
  *
  */
-@PluginInformation(pluginName = "Easing Animations", pluginUniqueName = "easing-animations",
-        pluginDescription = "",
-        pluginVersion = "1.4.1",
-        pluginDependancyUniqueIDs = "jquery", pluginCategories = "animation, jquery,easing",
-        pluginSubtitle = "A jQuery plugin from GSGD to give advanced easing options.",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQEasingEffects",
-        pluginSourceUrl = "",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQEasingEffects/wiki",
-        pluginOriginalHomepage = "http://gsgd.co.uk/sandbox/jquery/easing/")
+@PluginInformation(pluginName = "Easing Animations",
+                   pluginUniqueName = "easing",
+                   pluginDescription = "Animations are a huge part of making compelling web applications and sites. Users have come to expect highly responsive and interactive user interfaces. Animating your interface, however, is not necessarily straightforward. What should be animated, when, and what kind of feel should the animation have?"
+                   + "<br/>In classic animation, the term for motion that starts slowly and accelerates is \"slow in,\" and for motion that starts quickly and decelerates is \"slow out.\" The terminology most commonly used on the web for these are “ease in” and “ease out,” respectively. Sometimes the two are combined, which is called \"ease in out.\" Easing, then, is really the process of making the animation less severe or pronounced.",
+                   pluginVersion = "1.4.1",
+                   pluginDependancyUniqueIDs = "jquery",
+                   pluginCategories = "animation, jquery,easing",
+                   pluginSubtitle = "A jQuery plugin from GSGD to give advanced easing options.",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQEasingEffects",
+                   pluginSourceUrl = "http://gsgd.co.uk/sandbox/jquery/easing/",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQEasingEffects/wiki",
+                   pluginOriginalHomepage = "http://easings.net/",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/JQEasingEffects.jar/download",
+                   pluginIconUrl = "bower_components/jquery-easing/icon.jpg",
+                   pluginIconImageUrl = "bower_components/jquery-easing/example.jpg",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class EasingPageConfigurator extends PageConfigurator
 {
 
