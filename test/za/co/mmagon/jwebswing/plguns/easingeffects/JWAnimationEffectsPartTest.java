d494c8c8d010badedb0e3ea8ac3077d02feadb56
Marc Magon <ged_marc@hotmail.com>
2017-09-28 03:56:26 +0200

    JUnit 5 updates, Pretty Checkboxes implementation and a lot more
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plguns/easingeffects/JWAnimationEffectsPartTest.java
+++ test/za/co/mmagon/jwebswing/plguns/easingeffects/JWAnimationEffectsPartTest.java
@@ -16,7 +16,7 @@
  */
 package za.co.mmagon.jwebswing.plguns.easingeffects;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import za.co.mmagon.jwebswing.generics.Direction;
 import za.co.mmagon.jwebswing.plugins.easingeffects.JQEasingAnimationEffectsPart;
 import za.co.mmagon.jwebswing.plugins.easingeffects.JQEasingEffects;
