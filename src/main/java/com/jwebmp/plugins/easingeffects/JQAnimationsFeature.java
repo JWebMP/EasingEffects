27bd530b74d182f6f79a57a89f358c7ef8d41832
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:01:11 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/easingeffects/JQAnimationsFeature.java
+++ src/main/java/com/jwebmp/plugins/easingeffects/JQAnimationsFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@ import com.jwebmp.core.htmlbuilder.javascript.JavaScriptPart;
 import com.jwebmp.core.plugins.ComponentInformation;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  */
 @ComponentInformation(name = "Easing Animations ",
 		description = "Specify an easing animation for a component",
