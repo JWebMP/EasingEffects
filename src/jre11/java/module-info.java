7968799dbaa193cb5040741c1bd03b6481aa217c
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:43 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -11,7 +11,6 @@ module com.jwebmp.plugins.easingeffects {
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.easingeffects.JQEasingPageConfigurator;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.easingeffects.implementations.EasingEffectsExclusionsModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.easingeffects.implementations.EasingEffectsExclusionsModule;
 
 	opens com.jwebmp.plugins.easingeffects to com.fasterxml.jackson.databind, com.jwebmp.core;
